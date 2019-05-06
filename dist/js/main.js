2ebe33d0eb248ae90c3003afe49685c146f1cf0b
Erik Brown <hello@erikbrown.dev>
2019-05-06 14:14:20 -0400

    initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,39 @@
+{
+  /* Add Map */
+}
+function initMap() {
+  // Your Location
+  const loc = { lat: 36.099861, lng: -80.244217 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector(".map"), {
+    zoom: 14,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
+
+// Sticky menu background
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
+$("#main-nav a, .btn").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
